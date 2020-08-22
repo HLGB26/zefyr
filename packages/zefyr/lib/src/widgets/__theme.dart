8cfed6ca063cd6c2a75ec5f675202a33660890f7
harshlohia <57151106+harshlohia@users.noreply.github.com>
2020-08-22 18:17:44 +0530

    new

--- packages/zefyr/lib/src/widgets/__theme.dart
+++ packages/zefyr/lib/src/widgets/__theme.dart
@@ -251,7 +251,7 @@ class BlockTheme {
         textStyle: TextStyle(
           color: defaultTextStyle.style.color.withOpacity(0.8),
           fontFamily: fontFamily,
-          fontSize: 10.0,
+          fontSize: 2.0,
           height: 1.25,
         ),
         padding: padding,
